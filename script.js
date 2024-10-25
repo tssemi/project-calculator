999a96efa508f174e9a2244a43aa6cfff4d82959
Tssemi <127999956+tssemi@users.noreply.github.com>
2024-10-25 19:07:23 -0300

    Clear script file. Create div elements to store the symbols

--- script.js
+++ script.js
@@ -7,18 +7,14 @@ let num1 = 4;
 let operator = '-';
 let num2 = 2;
 
-//Create a new function operate
 function operate(n1, n2, op) {
     switch (op) {
-        case '+':
-            return add(n1, n2)
-        case '-':
-            return subtract(n1, n2)
-        case '*':
-            return multiply(n1, n2)
-        case '/':
-            return divide(n1, n2)
+        case '+': return add(n1, n2)
+        case '-': return subtract(n1, n2)
+        case '*': return multiply(n1, n2)
+        case '/': return divide(n1, n2)
     }
 }
 
-console.log(operate(num1, num2, operator))
+console.log(operate(num1, num2, operator));
+
