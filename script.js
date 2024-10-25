b4cd8128c14ea52c4e70a86f432f4a046a09b4f4
Tssemi <127999956+tssemi@users.noreply.github.com>
2024-10-25 18:41:51 -0300

    Create a new function that calls the previous ones

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function add(a, b) {a + b}
-function subtract(a, b) {a - b}
-function multiply(a, b) {a * b}
-function divide(a, b) {a / b}
+function add(a, b) {return a + b}
+function subtract(a, b) {return a - b}
+function multiply(a, b) {return a * b}
+function divide(a, b) {return a / b}
 
-//create variables
-let num1 = 0;
-let operator = '';
-let num2 = 0;
+let num1 = 4;
+let operator = '-';
+let num2 = 2;
+
+//Create a new function operate
+function operate(n1, n2, op) {
+    switch (op) {
+        case '+':
+            return add(n1, n2)
+        case '-':
+            return subtract(n1, n2)
+        case '*':
+            return multiply(n1, n2)
+        case '/':
+            return divide(n1, n2)
+    }
+}
+
+console.log(operate(num1, num2, operator))
