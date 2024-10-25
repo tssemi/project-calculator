4a82f8f1079e4d71a9af2dcb29f2b051966b2ca3
Tssemi <127999956+tssemi@users.noreply.github.com>
2024-10-25 18:26:40 -0300

    create functions of basic mathematical operators

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-//create functions
 function add(a, b) {a + b}
 function subtract(a, b) {a - b}
 function multiply(a, b) {a * b}
 function divide(a, b) {a / b}
+
+//create variables
+let num1 = 0;
+let operator = '';
+let num2 = 0;
