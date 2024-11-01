bc4da7fc1eecb7c586d76e4caab6dd4f7fc49511
Tssemi <127999956+tssemi@users.noreply.github.com>
2024-11-01 09:55:56 -0300

    Delete all event listeners and create a new one. This time only one

--- script.js
+++ script.js
@@ -9,54 +9,19 @@ function operate(n1, n2, op) {
         case '-': return subtract(n1, n2)
         case '*': return multiply(n1, n2)
         case '/': return divide(n1, n2)
-        default : return n1
+        default : return n1 == 0 ? n2 : n1
     }
 }
 
-let numbers = document.querySelectorAll('.digit');
-let operators = document.querySelectorAll('.operator');
+const calculator = document.querySelectorAll('.calculator')
 
 let bar = document.createElement('div');
 let num1 = 0;
-let num2;
+let num2 = 0;
 let operator = '';
 
-apdChild(num1, 0)
+for (let ele in calculator) {
+    ele.addEventListener('click', (e) => {
 
-function apdChild(ele, val) {
-    ele += val;
-    bar.textContent = ele;
-}
-function deleteValues(...ele) {ele.map(e => e = 0)}
-
-function callFunctions() {
-    let n = operate(+num1, +num2 == '' ? +num1 : +num2, operator);
-    while (bar.firstChild) {
-        bar.removeChild(bar.firstChild);
-    }
-    deleteValues(num1, num2);
-    operator = '';
-    bar.textContent = n;
-}
-for (let num of numbers) {
-    num.addEventListener('click', (e) => {
-        if (bar.textContent.length < 10) {
-            num1 += e.target.textContent;
-            bar.textContent = num1;
-            callFunctions();
-        }
     })
-}
-
-for (let op of operators) {
-    op.addEventListener('click', (e) => {
-        if (operator.length > 0 || e.target.textContent === '=') { 
-            callFunctions();
-        }
-        if (e.target.textContent !== '=') operator = e.target.textContent;
-        console.log(operator)
-    })
-}
-
-const calculator = document.querySelector('.calculator')
-calculator.insertBefore(bar, calculator.firstChild).classList.add('bar')
+} 
