d93c64692ce8041042bc1560825ef91118656d27
Tssemi <127999956+tssemi@users.noreply.github.com>
2024-11-02 13:10:18 -0300

    Add conditionals to the function

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function divide(a, b) {return a / b}
 
 function operate(n1, n2, op) {
     switch (op) {
-        case '+': return add(n1, n2)
-        case '-': return subtract(n1, n2)
-        case '*': return multiply(n1, n2)
-        case '/': return divide(n1, n2)
-        default : return n1 == 0 ? n2 : n1
+        case '+': return add(+n1, +n2)
+        case '-': return subtract(+n1, +n2)
+        case '*': return multiply(+n1, +n2)
+        case '/': return divide(+n1, +n2)
+        default : return +n1
     }
 }
 
@@ -22,15 +22,18 @@ let bar = document.createElement('div');
 bar.classList.add('bar');
 bar.textContent = num1;
 
-
 function storeValues(val) {
     if (Number.isInteger(+val)) {
         if (operator === '') {
-            num1 += +val;
-            bar.textContent += val;  
+            num1 += val;
+            bar.textContent = num1;
         } else {
-            num2 += +val;
-            bar.textContent += val;
+            num2 += val;
+            bar.textContent = num2;
+        }
+        console.log('num2 ' + num2)
+        if (num2 !== '') {
+            console.log(operate(num1, num2, operator))  
         }
     } else {
         operator = val;
@@ -42,23 +45,7 @@ function storeValues(val) {
 
 for (let ele of calculator) {
     ele.addEventListener('click', (e) => {
-        let n = '';
-        if (Number.isInteger(e.target.textContent)) {
-            n += e.target.textContent;
-            storeValues(n)
-        } else {
-            switch (e.target.textContent) {
-                case 'CL':
-                    break;
-                case '=':
-                    let n = operate(num1 == '' ? 0 : +num1, num2 == '' ? 0 : +num2, operator)
-                    
-                    break;
-                default:
-                    storeValues(e.target.textContent)
-                    break;
-            }
-        }
+            storeValues(e.target.textContent)
     })
 }
 
